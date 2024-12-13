52aa1ca10643e05d982aa6a65f773648b183fbd8
WendelHime <6754291+WendelHime@users.noreply.github.com>
2024-12-13 10:32:13 -0300

    fix: adding dialer docs and making Logger parameter optional

--- dialer/dialer.go
+++ dialer/dialer.go
@@ -1,3 +1,4 @@
+// Package dialer holds the dialer implementation for the water transport.
 package dialer
 
 import (
@@ -7,18 +8,27 @@ import (
 	"github.com/getlantern/golog"
 	"github.com/getlantern/lantern-water/logger"
 	"github.com/refraction-networking/water"
+	_ "github.com/refraction-networking/water/transport/v1"
 )
 
+// DialerParameters are used when creating a new dialer.
 type DialerParameters struct {
+	// An optional golog.Logger used for keeping compatibility with http-proxy
+	// and flashlight logger. If not defined the dialer will use the default
+	// water logger.
 	Logger    golog.Logger
-	Transport string
-	WASM      []byte
+	Transport string // Specifies transport being used.
+	WASM      []byte // The WASM module to use.
 }
 
+// NewDialer creates a new water dialer with the given parameters.
 func NewDialer(ctx context.Context, params DialerParameters) (water.Dialer, error) {
 	cfg := &water.Config{
 		TransportModuleBin: params.WASM,
-		OverrideLogger:     slog.New(logger.NewLogHandler(params.Logger, params.Transport)),
+	}
+
+	if params.Logger != nil {
+		cfg.OverrideLogger = slog.New(logger.NewLogHandler(params.Logger, params.Transport))
 	}
 
 	dialer, err := water.NewDialerWithContext(ctx, cfg)
